2f6c18517174135790e7d56082af30cf98bb7705
hopkinsjohn <37079215+hopkinsjohn@users.noreply.github.com>
2026-02-20 16:22:45 -0500

    commit 2

--- controls.lua
+++ controls.lua
@@ -24,17 +24,88 @@ end
 for i = 1, iMaxGains do
   ------------------------------- gain index
   table.insert(ctrls, {             
-    Name          = "Gain "..i.." Index",
-    ControlType   = "Indicator",
-    IndicatorType = "Text",
-    UserPin       = false,
-  })
+    Name            = "Gain "..i.." Index",
+    ControlType     = "Indicator",
+    IndicatorType   = "Text",
+    UserPin         = false,
+  })  
   ------------------------------- gain friendly name
   table.insert(ctrls, {             
-    Name          = "Gain "..i.." Friendly Name",
-    ControlType   = "Text",
-    UserPin       = true,
-    PinStyle      = "Both",
+    Name            = "Gain "..i.." Friendly Name",
+    ControlType     = "Text",
+    UserPin         = true,
+    PinStyle        = "Both",
+  })
+  ------------------------------- gain friendly name display
+  table.insert(ctrls, {             
+    Name            = "Gain "..i.." Friendly Name Display",
+    ControlType     = "Indicator",
+    IndicatorType   = "Text",
+    UserPin         = true,
+    PinStyle        = "Output",
+  })
+  ------------------------------- gain fader
+  table.insert(ctrls, {             
+    Name            = "Gain "..i.." Fader",
+    ControlType     = "Knob",
+    ControlUnit     = "dB",
+    Min             = GainMinTbl[i],
+    Max             = GainMaxTbl[i],
+    UserPin         = true,
+    PinStyle        = "Both",
+  })
+  ------------------------------- max gain display
+  table.insert(ctrls, {             
+    Name            = "Gain "..i.." Display Max",
+    ControlType     = "Indicator",
+    IndicatorType   = "Text",
+    UserPin         = false,
+  })
+  ------------------------------- min gain display
+  table.insert(ctrls, {             
+    Name            = "Gain "..i.." Display Min",
+    ControlType     = "Indicator",
+    IndicatorType   = "Text",
+    UserPin         = false,
+  })
+  
+  ------------------------------- gain up trigger
+  table.insert(ctrls, {
+    Name            = "Gain "..i.." Bump Up",
+    ControlType     = "Button",
+    ButtonType      = "Trigger",
+    UserPin         = true,
+    PinStyle        = "Both",
+    IconType        = "Icon",
+    Icon            = "Plus",
+  })
+  ------------------------------- gain down trigger
+  table.insert(ctrls, {
+    Name            = "Gain "..i.." Bump Down",
+    ControlType     = "Button",
+    ButtonType      = "Trigger",
+    UserPin         = true,
+    PinStyle        = "Both",
+    IconType        = "Icon",
+    Icon            = "Minus",
+  })
+  ------------------------------- mute toggle
+  table.insert(ctrls, {
+    Name            = "Gain "..i.." Mute Toggle",
+    ControlType     = "Button",
+    ButtonType      = "Toggle",
+    UserPin         = true,
+    PinStyle        = "Both",
+  })
+  ------------------------------- mute state trigger
+  table.insert(ctrls, {
+    Name            = "Gain "..i.." Mute State Trigger",
+    ControlType     = "Button",
+    ButtonType      = "StateTrigger",
+    Min             = 1,
+    Max             = 4,
+    UserPin         = true,
+    PinStyle        = "Both",
   })
 
 
--- layout.lua
+++ layout.lua
@@ -8,15 +8,17 @@ layout["code"]={PrettyName="code",Style="None"}
 
 local GroupBoxFillColor = {51,51,51}
 local GroupBoxStrokeColor = {125,198,35}
-local StandardWidth = 50
+local StandardWidth = 60
 local StandardHeight = 20
-local PositionStartX = 123
+local FaderHeight = 200
+local PositionStartX = 123 - StandardWidth
 local PositionStartY = 8
 local TextFont    = "Montserrat"
 local TextFontStyle = "Regular"
-local TextColorBlack = {0,0,0}
-local TextColorGreen = {125,198,35}
-local TextColorSilver = {204,204,204} 
+local ColorBlack = {0,0,0}
+local ColorGreen = {125,198,35}
+local ColorSilver = {204,204,204} 
+local ColorButtonOff = {102,102,102} 
 
 local iMaxGains = math.floor(props["Number Of Gains"].Value)
 local CurrentPage = PageNames[props["page_index"].Value] -- gathers the name of the current page
@@ -30,7 +32,7 @@ if CurrentPage == "Control" then
     StrokeColor   = GroupBoxStrokeColor,
     StrokeWidth   = 1,
     Position      = {0,0},
-    Size          = {PositionStartX + (iMaxGains*StandardWidth) + 60,391},
+    Size          = {PositionStartX + StandardWidth + (iMaxGains*StandardWidth) + 60,391},
     ZOrder        = -40,
   })
 
@@ -43,36 +45,142 @@ if CurrentPage == "Control" then
   --------------------------- otherstuff
   
   for i = 1, iMaxGains do
-  --------------------------- Label - channel index
-  layout["Gain "..i.." Index"] = { 
-    PrettyName    = "Gain "..i.." Index",
-    Style         = "Text",
-    TextBoxStyle  = "NoBackground",
-    Position      = {PositionStartX + (i-1)*StandardWidth, PositionStartY},
-    Size          = {StandardWidth,StandardHeight},
-    FontSize      = 9,
-    Margin        = 0,
-    Font          = TextFont,
-    FontStyle     = TextFontStyle,
-    TextColor     = TextColorGreen,
-    HTextAlign    = "Center",
-    IsReadOnly    = true,
-  }
-  --------------------------- gain friendly names
-  layout["Gain "..i.." Friendly Name"] = { 
-    PrettyName    = string.format("Gain %i~Friendly Name",i),
-    Style         = "Text",
-    Position      = {PositionStartX + (i-1)*StandardWidth , PositionStartY + StandardHeight},
-    Size          = {StandardWidth,StandardHeight},
-    Color         = {255,255,255},
-    FontSize      = 12,
-    Font          = TextFont,
-    FontStyle     = TextFontStyle,
-    TextColor     = TextColorBlack,
-    Margin        = 0,
-    HTextAlign    = "Center",
-    IsReadOnly    = false,
-  }
+    -- iterate some variables
+    PositionStartX = PositionStartX + StandardWidth
+    PositionStartY = 8
+    --------------------------- Label - channel index
+    layout["Gain "..i.." Index"] = { 
+      PrettyName    = "Gain "..i.." Index",
+      Style         = "Text",
+      TextBoxStyle  = "NoBackground",
+      Position      = {PositionStartX , PositionStartY},
+      Size          = {StandardWidth , StandardHeight},
+      FontSize      = 9,
+      Margin        = 0,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      TextColor     = ColorGreen,
+      HTextAlign    = "Center",
+      IsReadOnly    = true,
+    }
+    PositionStartY = PositionStartY + StandardHeight
+    --------------------------- gain friendly names
+    layout["Gain "..i.." Friendly Name"] = { 
+      PrettyName    = string.format("Gain %i~Friendly Name",i),
+      Style         = "Text",
+      Position      = {PositionStartX, PositionStartY},
+      Size          = {StandardWidth , StandardHeight},
+      Color         = {255,255,255},
+      FontSize      = 9,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      TextColor     = ColorBlack,
+      Margin        = 0,
+      HTextAlign    = "Center",
+      IsReadOnly    = false,
+    }
+    PositionStartY = PositionStartY + StandardHeight
+    --------------------------- gain friendly names display
+    layout["Gain "..i.." Friendly Name Display"] = { 
+      PrettyName    = string.format("Gain %i~Friendly Name Display",i),
+      Style         = "Text",
+      TextBoxStyle  = "Normal",
+      Position      = {PositionStartX , PositionStartY},
+      Size          = {StandardWidth , StandardHeight},
+      Color         = {194,194,194},
+      FontSize      = 9,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      TextColor     = ColorBlack,
+      Margin        = 0,
+      HTextAlign    = "Center",
+      IsReadOnly    = true,
+    }
+    PositionStartY = PositionStartY + StandardHeight
+    --------------------------- gain faders
+    layout["Gain "..i.." Fader"] = { 
+      PrettyName    = string.format("Gain %i~Fader",i),
+      Style         = "Fader",
+      Position      = {PositionStartX + 5 , PositionStartY},
+      Size          = {StandardWidth - 10 , FaderHeight},
+      Color         = ColorGreen,
+    }
+    --------------------------- Label - Max Gain Display
+    layout["Gain "..i.." Display Max"] = { 
+      PrettyName    = string.format("Gain %i~Max dB",i),
+      Style         = "Text",
+      TextBoxStyle  = "NoBackground",
+      Position      = {PositionStartX , PositionStartY},
+      Size          = {StandardWidth,14},
+      FontSize      = 9,
+      Margin        = 0,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      TextColor     = ColorButtonOff,
+      HTextAlign    = "Center",
+      IsReadOnly    = true,
+      ZOrder        = -39,
+    }
+    PositionStartY = PositionStartY + FaderHeight
+    --------------------------- Label - Min Gain Display
+    layout["Gain "..i.." Display Min"] = { 
+      PrettyName    = string.format("Gain %i~Min dB",i),
+      Style         = "Text",
+      TextBoxStyle  = "NoBackground",
+      Position      = {PositionStartX , PositionStartY - 14},
+      Size          = {StandardWidth,14},
+      FontSize      = 9,
+      Margin        = 0,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      TextColor     = ColorButtonOff,
+      HTextAlign    = "Center",
+      IsReadOnly    = true,
+      ZOrder        = -38,
+    }
+    --------------------------- Gain Bump Up
+    -- Channel Selection
+    layout["Gain "..i.." Bump Up"] = {
+      PrettyName    = string.format("Gain %i~Bump Up",i),
+      Style         = "Button",
+      Position      = {PositionStartX , PositionStartY},
+      Size          = {30,StandardHeight},
+      Margin        = 1,
+      CornerRadius  = 0,
+      StrokeWidth   = 0,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      Color         = ColorGreen,
+      OffColor      = GroupBoxFillColor,
+      ButtonVisualStyle = "Flat",
+      UnlinkOffColor = true,
+      Padding       = 2,
+      IconColor     = ColorGreen,
+    }
+    --------------------------- Gain Bump Down
+    -- Channel Selection
+    layout["Gain "..i.." Bump Down"] = {
+      PrettyName    = string.format("Gain %i~Bump Down",i),
+      Style         = "Button",
+      Position      = {PositionStartX + 30 , PositionStartY},
+      Size          = {30,StandardHeight},
+      Margin        = 1,
+      CornerRadius  = 0,
+      StrokeWidth   = 0,
+      Font          = TextFont,
+      FontStyle     = TextFontStyle,
+      Color         = ColorGreen,
+      OffColor      = GroupBoxFillColor,
+      ButtonVisualStyle = "Flat",
+      UnlinkOffColor = true,
+      Padding       = 2,
+      IconColor     = ColorGreen,
+    }
+
+
+
+
+
 
   end
 
