d130bc66a52eea20d4c6f61c9195663985d878bd
hopkinsjohn <37079215+hopkinsjohn@users.noreply.github.com>
2026-04-20 10:13:37 -0400

    name change

--- info.lua
+++ info.lua
@@ -1,8 +1,8 @@
 PluginInfo = {
-  Name = "OnyxAV~Gain Assist v2",
+  Name = "OnyxAV~Gain Assist",
   Version = "1.0",
-  BuildVersion = "0.0.0.180",
+  BuildVersion = "0.0.0.181",
   Id = "3ab371d4-87ee-4b6b-9c6f-bd99203a0851",
   Author = "jhopkins",
-  Description = "Gain Assist"  
+  Description = "OnyxAV Gain Assist"  
 }
