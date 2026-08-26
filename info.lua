5f1ccd93d695858973d38bc97af6ba6467cdd4a1
hopkinsjohn <37079215+hopkinsjohn@users.noreply.github.com>
2026-08-26 16:11:26 -0400

    finalized after max execution fix

--- info.lua
+++ info.lua
@@ -1,7 +1,7 @@
 PluginInfo = {
   Name = "OnyxAV~OAV Gain Assist v2.2",
   Version = "2.2",
-  BuildVersion = "0.0.0.90",
+  BuildVersion = "0.0.0.91",
   Id = "84b36ad0-6c7a-4e9f-be8a-b185e0461da2",
   Author = "jhopkins",
   Description = "OnyxAV Gain Assist v2"  
